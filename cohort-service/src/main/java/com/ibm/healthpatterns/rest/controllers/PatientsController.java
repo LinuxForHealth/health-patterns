d1574efad44b6ead540daa29c75911b39c6d83c7
luisgarcc <luisg.dev1@gmail.com>
2021-01-26 14:04:07 -0700

    New service to return the whole FHIR patient resource
    This commit adds a new service to return the whole FHIR patient
    resource, not just the patient IDs.

--- cohort-service/src/main/java/com/ibm/healthpatterns/rest/controllers/PatientsController.java
+++ cohort-service/src/main/java/com/ibm/healthpatterns/rest/controllers/PatientsController.java
@@ -1,8 +1,11 @@
 package com.ibm.healthpatterns.rest.controllers;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ibm.healthpatterns.app.CQLExecutionException;
 import com.ibm.healthpatterns.app.CohortService;
 
 /**
@@ -38,9 +42,14 @@ public class PatientsController {
 	 * @param id the library id
 	 * @return the list of patients
 	 */
-	@GetMapping("/patients")
-	public @ResponseBody ResponseEntity<String> getPatients(@PathVariable String id) {
-		List<String> cohort = cohortService.getPatients(id);
+	@GetMapping("/patientIDs")
+	public @ResponseBody ResponseEntity<String> getPatientIds(@PathVariable String id) {
+		List<String> cohort;
+		try {
+			cohort = cohortService.getPatientIdsForCohort(id);
+		} catch (CQLExecutionException e) {
+			return cqlExecutionExceptionResponse(e);
+		}
 		if (cohort == null) {
 			return new ResponseEntity<String>("Library with ID '" + id + "' was not found.", HttpStatus.NOT_FOUND);
 		}
@@ -51,7 +60,36 @@ public class PatientsController {
 		} catch (JsonProcessingException e) {
 			return new ResponseEntity<String>("Could not serialize cohort: " + e, HttpStatus.INTERNAL_SERVER_ERROR);	
 		}
-		return new ResponseEntity<String>(json, HttpStatus.OK);
+		return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(json);
+	}
 
+	/**
+	 * 
+	 * @param id the library id
+	 * @return the list of patients
+	 */
+	@GetMapping("/patients")
+	public @ResponseBody ResponseEntity<String> getPatients(@PathVariable String id) {
+		String fhirResponse;
+		try {
+			fhirResponse = cohortService.getPatientsForCohort(id);
+		} catch (CQLExecutionException e) {
+			return cqlExecutionExceptionResponse(e);
+		}
+		if (fhirResponse == null) {
+			return new ResponseEntity<String>("Library with ID '" + id + "' was not found.", HttpStatus.NOT_FOUND);
+		}
+		return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(fhirResponse);
+	}
+
+	private ResponseEntity<String> cqlExecutionExceptionResponse(CQLExecutionException e) {
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		pw.println("Error in the Cohort Engine executing the given library.");
+		pw.println();
+		e.printStackTrace(pw);
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+				.contentType(MediaType.TEXT_PLAIN)
+				.body(sw.toString());
 	}
 }
