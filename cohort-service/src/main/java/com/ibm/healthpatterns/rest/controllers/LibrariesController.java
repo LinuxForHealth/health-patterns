dedd981e64ccb70ac01b81837b734e5deec6b5d3
luisgarcc <luisg.dev1@gmail.com>
2021-01-27 12:32:32 -0700

    Add copyrights
    This commit updates comments and add copyrights.

--- cohort-service/src/main/java/com/ibm/healthpatterns/rest/controllers/LibrariesController.java
+++ cohort-service/src/main/java/com/ibm/healthpatterns/rest/controllers/LibrariesController.java
@@ -1,3 +1,21 @@
+/*
+ * (C) Copyright IBM Corp. 2021
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.ibm.healthpatterns.rest.controllers;
 
 import java.io.IOException;
@@ -25,8 +43,7 @@ import com.ibm.healthpatterns.app.CohortService;
 /**
  * The controller that handles the library services (CRUD operations for libraries).
  *  
- * @author Luis A. Garía
- *
+ * @author Luis A. García
  */
 @RestController
 @RequestMapping("/cohort-service/libraries")
