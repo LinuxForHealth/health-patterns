7f0a0ad08dcb71de43f04e1163fbd19f0bc31b52
luisgarcc <luisg.dev1@gmail.com>
2021-02-05 11:32:14 -0700

    Enhancement to the DeID Custom Processor
    This commit includes the following enhancements:
    
    - New connection property for the DeID service
    - Adjust existing properties for name consistency
    - FHIR username/password are no longer required
    - Change visibility of properties and relationships to package
    - Add validation of the connection properties using deid.healethcheck()
    - Better logging
    - Fix bug parsing FHIR response when deiding FHIR resources
    - Clean up and constant extraction
    
    Additionally this commit includes NiFi TestRunner test case which
    exercises the custom processor within a mock NiFi environment which
    greatly helps identify bugs that would have been caught by deploying to
    NiFi.

--- nifi-processors/nifi-health-patterns-bundle/nifi-health-patterns-processors/src/main/java/com/ibm/healthpatterns/processors/deid/DeIdentifyAndPostToFHIR.java
+++ nifi-processors/nifi-health-patterns-bundle/nifi-health-patterns-processors/src/main/java/com/ibm/healthpatterns/processors/deid/DeIdentifyAndPostToFHIR.java
@@ -22,8 +22,10 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -34,14 +36,17 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 
+import org.apache.http.HttpHeaders;
+import org.apache.http.entity.ContentType;
 import org.apache.nifi.annotation.behavior.InputRequirement;
 import org.apache.nifi.annotation.behavior.InputRequirement.Requirement;
 import org.apache.nifi.annotation.behavior.WritesAttribute;
 import org.apache.nifi.annotation.behavior.WritesAttributes;
 import org.apache.nifi.annotation.documentation.CapabilityDescription;
 import org.apache.nifi.annotation.documentation.Tags;
-import org.apache.nifi.annotation.lifecycle.OnScheduled;
 import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.components.ValidationContext;
+import org.apache.nifi.components.ValidationResult;
 import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.logging.ComponentLog;
 import org.apache.nifi.processor.AbstractProcessor;
@@ -69,54 +74,77 @@ import com.ibm.healthpatterns.deid.DeIdentifierException;
 @CapabilityDescription("De-identifies the given FHIR resources and adds them to a designated FHIR server.")
 @InputRequirement(Requirement.INPUT_REQUIRED)
 @WritesAttributes({
-    @WritesAttribute(attribute = "location", description = "The HTTP Location header returned by the FHIR server when a resource is created"),
-    @WritesAttribute(attribute = "deidTransactwionId", description = "All FlowFiles produced from the deidentifying and persisting the same parent FlowFile will have the same randomly generated UUID added for this attribute")})
+    @WritesAttribute(attribute = "Location", description = "The HTTP Location header returned by the FHIR server when a resource is created"),
+    @WritesAttribute(attribute = "deidTransactionID" , description = "All FlowFiles produced from the deidentifying and persisting the same parent FlowFile will have the same randomly generated UUID added for this attribute")})
 public class DeIdentifyAndPostToFHIR extends AbstractProcessor {
 
-	private static final PropertyDescriptor FHIR_URL = new PropertyDescriptor.Builder()
-			.name("Deidentified FHIR Server URL")
-			.description("The URL of the FHIR server that will be used to persist the deidentified FHIR resources.")
+	/**
+	 * The MIME Type attribute
+	 */
+	static final String MIME_TYPE_ATTRIBUTE = "mime.type";
+
+	/**
+	 * The deid transaction atribute
+	 */
+	static final String DEID_TRANSACTION_ID_ATTRIBUTE = "deidTransactionID";
+
+	/**
+	 * The location header attribute
+	 */
+	static final String LOCATION = HttpHeaders.LOCATION;
+
+	static final PropertyDescriptor DEID_URL = new PropertyDescriptor.Builder()
+			.name("De-identification Service URL")
+			.description("The full base URL of the de-identeification service API, e.g.: http://ingestion-deid/api/v1")
 			.required(true)
-			.defaultValue("http://ingestion-fhir-deid/fhir-server/api-v4")
+			.defaultValue("http://ingestion-deid:8080/api/v1")
 			.addValidator(StandardValidators.URI_VALIDATOR)
 			.build();
 
-	private static final PropertyDescriptor FHIR_USERNAME = new PropertyDescriptor.Builder()
-			.name("Deidentified FHIR Server Username")
-			.description("The username of the FHIR server that will be used to persist the deidentified FHIR resources.")
+	static final PropertyDescriptor FHIR_URL = new PropertyDescriptor.Builder()
+			.name("De-identified FHIR Server URL")
+			.description("The full base URL of the FHIR server that will be used to persist the deidentified FHIR resources, e.g.: http://ingestion-fhir-deid/fhir-server/api/v4")
 			.required(true)
+			.defaultValue("http://ingestion-fhir-deid/fhir-server/api/v4")
+			.addValidator(StandardValidators.URI_VALIDATOR)
+			.build();
+
+	static final PropertyDescriptor FHIR_USERNAME = new PropertyDescriptor.Builder()
+			.name("De-identified FHIR Server Username")
+			.description("The username of the FHIR server that will be used to persist the deidentified FHIR resources.")
+			.required(false)
 			.defaultValue("fhiruser")
 			.addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
 			.build();
 
-	private static final PropertyDescriptor FHIR_PASSWORD = new PropertyDescriptor.Builder()
-			.name("Deidentified FHIR Server Password")
+	static final PropertyDescriptor FHIR_PASSWORD = new PropertyDescriptor.Builder()
+			.name("De-identified FHIR Server Password")
 			.description("The password of the FHIR server that will be used to persist the deidentified FHIR resources.")
-			.required(true)
+			.required(false)
 			.sensitive(true)
 			.defaultValue("integrati0n")
 			.addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
 			.build();
 
-	private static final Relationship ORIGINAL = new Relationship.Builder()
+	static final Relationship ORIGINAL = new Relationship.Builder()
 			.name("original")
 			.description("The original input file will be routed to this destination when it has been deidentified and"
 					+ "persisted to the DEID FHIR server.")
 			.build();
 
-	private static final Relationship DEIDENTIFIED = new Relationship.Builder()
+	static final Relationship DEIDENTIFIED = new Relationship.Builder()
 			.name("deidentified")
 			.description("The deidentified FHIR resource prior to it being persisted in the DEID FHIR server.")
 			.build();
 
-	private static final Relationship SUCCESS = new Relationship.Builder()
+	static final Relationship SUCCESS = new Relationship.Builder()
 			.name("success")
 			.description("The response from the FHIR server and if applicable the Location header of the new FHIR resource "
 					+ "will be sent here if the original FHIR resource was successfully deidentified and persisted to the "
 					+ "DEID FHIR server.")
 			.build();
 
-	private static final Relationship FAILURE = new Relationship.Builder()
+	static final Relationship FAILURE = new Relationship.Builder()
 			.name("failure")
 			.description("The original file will be routed here if there was a problem de-identifying the FHIR resource "
 					+ "or persisting it to the DEID FHIR server.")
@@ -126,6 +154,7 @@ public class DeIdentifyAndPostToFHIR extends AbstractProcessor {
 	private Set<Relationship> relationships;
 
 	private volatile DeIdentifier deid;
+	
 	/**
 	 * Initialize the processor with the four properties and two relationships defined above
 	 * @param context the processor context
@@ -133,6 +162,7 @@ public class DeIdentifyAndPostToFHIR extends AbstractProcessor {
 	@Override
 	protected void init(final ProcessorInitializationContext context) {
 		List<PropertyDescriptor> descriptors = new ArrayList<PropertyDescriptor>();
+		descriptors.add(DEID_URL);
 		descriptors.add(FHIR_URL);
 		descriptors.add(FHIR_USERNAME);
 		descriptors.add(FHIR_PASSWORD);
@@ -164,14 +194,30 @@ public class DeIdentifyAndPostToFHIR extends AbstractProcessor {
 		return descriptors;
 	}
 
-	/**
-	 * 
-	 * @param context
-	 */
-	@OnScheduled
-	public void onScheduled(final ProcessContext context) {
-		deid = new DeIdentifier();
-	}
+    @Override
+    protected Collection<ValidationResult> customValidate(final ValidationContext context) {
+        final List<ValidationResult> results = new ArrayList<>(super.customValidate(context));
+
+		String fhirURL = context.getProperty(FHIR_URL).getValue();
+		String fhirUsername = context.getProperty(FHIR_USERNAME).getValue();
+		String fhirPassword = context.getProperty(FHIR_PASSWORD).getValue();
+		String deidURL = context.getProperty(DEID_URL).getValue();
+		deid = new DeIdentifier(deidURL, fhirURL, fhirUsername, fhirPassword);
+
+		StringWriter status = new StringWriter();
+		boolean healthOK = deid.healthCheck(status);
+		
+		if (!healthOK) {
+	        ValidationResult validation = new ValidationResult.Builder()
+	        		.subject("The connection information")
+	        		.valid(false)
+	        		.explanation("the health checks did not pass:\n" + status)
+	        		.build();
+	        results.add(validation);			
+		}
+
+        return results;
+    }
 
 	/*
 	 * (non-Javadoc)
@@ -204,7 +250,7 @@ public class DeIdentifyAndPostToFHIR extends AbstractProcessor {
 					deidentification = deid.deIdentify(in);
 				} catch (DeIdentifierException e) {
 					error.set(true);
-					logger.error(e.getMessage() + " Routing to failure.", e)	;
+					logger.error("Error deidentifying flow file {} will route to failure: {}", new Object[]{flowFile, e.getMessage()}, e);
 				}
 				stopWatch.stop();
 				uploadMillis.set(stopWatch.getDuration(TimeUnit.MILLISECONDS));
@@ -217,13 +263,13 @@ public class DeIdentifyAndPostToFHIR extends AbstractProcessor {
 			session.transfer(flowFile, FAILURE);
 			return;
 		}
-		logger.info("Successfully de-identified and persisted to FHIR in {} at a rate of {}",
+		logger.info("Successfully de-identified and persisted flow file {} to FHIR in {} at a rate of {}",
 				new Object[]{flowFile, FormatUtils.formatMinutesSeconds(uploadMillis.get(), TimeUnit.MILLISECONDS), uploadDataRate.get()});
 
 		String transactionId = UUID.randomUUID().toString();
 		DeIdentification deidentification = deidentificationRef.get();
 
-		session.putAttribute(flowFile, "deidTransactionID", transactionId);
+		session.putAttribute(flowFile, DEID_TRANSACTION_ID_ATTRIBUTE, transactionId);
 		session.transfer(flowFile, ORIGINAL);
 		
 		JsonNode deidResource = deidentification.getDeIdentifiedResource();
@@ -241,8 +287,8 @@ public class DeIdentifyAndPostToFHIR extends AbstractProcessor {
 				}				
 			}
 		});
-		session.putAttribute(deidFlowFile, "deidTransactionID", transactionId);
-		session.putAttribute(deidFlowFile, "mime.type", "application/json");
+		session.putAttribute(deidFlowFile, DEID_TRANSACTION_ID_ATTRIBUTE, transactionId);
+		session.putAttribute(deidFlowFile, MIME_TYPE_ATTRIBUTE, ContentType.APPLICATION_JSON.getMimeType());
 		session.transfer(deidFlowFile, DEIDENTIFIED);
 
 		JsonNode fhirResponse = deidentification.getFhirResponse();
@@ -256,14 +302,29 @@ public class DeIdentifyAndPostToFHIR extends AbstractProcessor {
 			 */
 			@Override
 			public void process(OutputStream out) throws IOException {
+				// It is possible that the FHIR response body is null, when POSTing a single resource
+				// FHIR does not return anything in its body, but it does return a Location header
+				if (fhirResponse == null) {
+					return;
+				}
 				try (OutputStream outputStream = new BufferedOutputStream(out)) {
 					outputStream.write(fhirResponse.toPrettyString().getBytes(StandardCharsets.UTF_8));
 				}				
 			}
 		});
-		session.putAttribute(fhirResponseFlowFile, "deidTransactionID", transactionId);
-		session.putAttribute(fhirResponseFlowFile, "Location", fhirLLocationHeader);
-		session.putAttribute(deidFlowFile, "mime.type", "application/json");
+		session.putAttribute(fhirResponseFlowFile, MIME_TYPE_ATTRIBUTE, ContentType.APPLICATION_JSON.getMimeType());
+		session.putAttribute(fhirResponseFlowFile, DEID_TRANSACTION_ID_ATTRIBUTE, transactionId);
+		// The FHIR location header will be null if the resource posted was a Bundle
+		if (fhirLLocationHeader != null) {
+			session.putAttribute(fhirResponseFlowFile, LOCATION, fhirLLocationHeader);
+		}
 		session.transfer(fhirResponseFlowFile, SUCCESS);
+	}
+
+	/**
+	 * @return the deidentifiers used by this custom processor
+	 */
+	DeIdentifier getDeidentifier() {
+		return deid;
 	}
 }
