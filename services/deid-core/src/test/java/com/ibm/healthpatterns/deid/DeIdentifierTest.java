e36da0f36d99d1887aa5fd59db3af67e9b4b00b4
Adam T. Clark <atclark@us.ibm.com>
2021-04-13 16:48:16 -0500

    Add Ingress for Cohort. Update JUnits to target "git-test" namespace services.

--- services/deid-core/src/test/java/com/ibm/healthpatterns/deid/DeIdentifierTest.java
+++ services/deid-core/src/test/java/com/ibm/healthpatterns/deid/DeIdentifierTest.java
@@ -59,7 +59,7 @@ public class DeIdentifierTest extends FHIRServiceTest {
 	 * 
 	 */
 	public DeIdentifierTest() {
-		deidURL = "http://3a5d0fa4-us-south.lb.appdomain.cloud:8080/api/v1";
+		deidURL = "http://git-test.deid.integration-k8s-cluster-dcc48c44d831198cb8496b1ec68d7d12-0000.us-south.containers.appdomain.cloud/api/v1";
 		deid = new DeIdentifier(deidURL, fhirURL, fhirUsername, fhirPassword);
 	}
 
