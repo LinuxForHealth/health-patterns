9a208e6c98d9263cca375c5cb7317b12fb773a92
Bryce Hall <bhall@ibm.com>
2021-06-01 11:52:29 -0500

    Pulled inputstream handling code from the deidentify service

--- microprocessors/deid/src/main/java/org/alvearie/healthpatterns/DeIdentifyRest.java
+++ microprocessors/deid/src/main/java/org/alvearie/healthpatterns/DeIdentifyRest.java
@@ -1,23 +1,46 @@
 package org.alvearie.healthpatterns;
 
+import java.io.InputStream;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.ibm.fhir.model.*;
+import com.ibm.fhir.model.resource.Bundle;
 import com.ibm.fhir.model.resource.Resource;
+
+import org.jboss.logging.annotations.Param;
 
 
 @Path("/")
 public class DeIdentifyRest {
 
+    private ObjectMapper jsonDeserializer = new ObjectMapper();
+
+    // config params as optional url parameters?
+
+    // "database" of deid configs, select config via url param, if not use default.
+
     @POST
     @Path("deidentifyFHIR")
     @Consumes(MediaType.APPLICATION_JSON)
-    public String deidentifyFHIR(Resource resource) {
-        return resource.toString();
+    public String deidentifyFHIR(InputStream resourceInputStream) throws Exception {
+        JsonNode jsonNode;
+		try {
+			jsonNode = jsonDeserializer.readTree(resourceInputStream);
+		} catch (JsonParseException e) {
+			throw new Exception("The given input stream did not contain valid JSON.", e);
+		}
+		if (!(jsonNode instanceof ObjectNode)) {
+			throw new Exception("The FHIR resource did not contain a valid JSON object, likely it was a JSON array. Currently only proper FHIR resources are supported");
+		}
+        return jsonNode.toPrettyString();
     }
 
     @POST
@@ -25,6 +48,13 @@ public class DeIdentifyRest {
     @Consumes(MediaType.APPLICATION_JSON)
     public String deidentify() {
         return null;
+    }
+
+    @POST
+    @Path("postdeidconfig")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void setConfig() {
+
     }
 
 }
