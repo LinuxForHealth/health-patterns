3b17581b36a20124b99169ebbdf81ad354cc1048
kathrynelrod <elrodkathryn@gmail.com>
2021-06-07 09:59:02 -0400

    Added rudimentary terminology functionality - same error as deid

--- microservices/terminology/src/main/java/com/ibm/healthpatterns/microservices/terminology/TerminologyRest.java
+++ microservices/terminology/src/main/java/com/ibm/healthpatterns/microservices/terminology/TerminologyRest.java
@@ -17,6 +17,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.resteasy.annotations.jaxrs.QueryParam;
 
+@Path("/")
 public class TerminologyRest {
 
     @ConfigProperty(name = "FHIR_SERVER_URL")
@@ -27,5 +28,25 @@ public class TerminologyRest {
 
     @ConfigProperty(name = "FHIR_SERVER_PASSWORD")
     String FHIR_SERVER_PASSWORD;
+
+    private volatile TerminologyService terminologyService;
+
+    public TerminologyRest() {
+
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String translate(InputStream resourceInputStream) {
+        if (terminologyService == null) {
+            terminologyService = new TerminologyService(FHIR_SERVER_URL, FHIR_SERVER_USERNAME, FHIR_SERVER_PASSWORD);
+        }
+        try {
+            Translation result = terminologyService.translate(resourceInputStream);
+            return result.getTranslatedResource().toPrettyString();
+        } catch (Exception e) {
+            return e.toString();
+        }
+    }
 
 }
