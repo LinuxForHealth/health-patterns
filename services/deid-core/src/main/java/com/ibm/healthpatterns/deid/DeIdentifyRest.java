a20ea9ad656df0ec4c26353d8556ab5245e2119b
kathrynelrod <elrodkathryn@gmail.com>
2021-06-23 09:21:48 -0400

    Removed unnecessary import

--- services/deid-core/src/main/java/com/ibm/healthpatterns/deid/DeIdentifyRest.java
+++ services/deid-core/src/main/java/com/ibm/healthpatterns/deid/DeIdentifyRest.java
@@ -13,7 +13,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.jboss.resteasy.annotations.jaxrs.QueryParam;
 
 import org.jboss.logging.Logger;
 
