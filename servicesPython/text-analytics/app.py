d39bb356a68e4dc79dba0ac706a9e9d7db7acdbb
bitwise-knot <bhall.ia.99@gmail.com>
2021-06-28 10:25:07 -0500

    initial commit

---
+++ servicesPython/text-analytics/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/")
+def applyAnalytics():
+    return null
