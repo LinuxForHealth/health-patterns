245eabe4a00027103ececba66ac88bbaa63f9a6a
Adam T. Clark <atclark@us.ibm.com>
2021-06-29 13:48:11 -0500

    Update startHealthPatterns script to reflect new parameters

--- utilities/startHealthPatternsFlow.py
+++ utilities/startHealthPatternsFlow.py
@@ -217,18 +217,7 @@ def updateParameters(baseURL, fhir_password, kafka_password, releaseName, runASC
             update_parameter(baseURL, contextId, "FHIR_UserPwd_ProviderDirectory", fhir_password, True)
             update_parameter(baseURL, contextId, "kafka.auth.password", kafka_password, True)
             update_parameter(baseURL, contextId, "kafka.brokers", releaseName + "-kafka:9092", False)
-
-        if "De-Identification" in context["component"]["name"]:
-            if debug:
-                print("Processing De-Identification")
-            #set deid passwords
-            contextId = context["id"]
-            if debug:
-                print("DeId context: ", contextId)
-
-            update_parameter(baseURL, contextId, "DEID_FHIR_PASSWORD", fhir_password, True)
-            update_parameter(baseURL, contextId, "DEID_FHIR_SERVER", "http://" + releaseName + "-fhir-deid/fhir-server/api/v4", False)
-            update_parameter(baseURL, contextId, "DEID_SERVICE_API_URL", "http://" + releaseName + "-deid:8080/api/v1", False)
+            update_parameter(baseURL, contextId, "HL7_RESOURCE_GENERATOR_URL", "http://" + releaseName + "-hl7-resource-generator:8080/hl7/transformation", False)
 
         if "Enrichment" in context["component"]["name"]:
             if debug:
@@ -240,6 +229,8 @@ def updateParameters(baseURL, fhir_password, kafka_password, releaseName, runASC
 
             update_parameter(baseURL, contextId, "kafka.auth.password", kafka_password, True)
             update_parameter(baseURL, contextId, "kafka.brokers", releaseName + "-kafka:9092", False)
+            update_parameter(baseURL, contextId, "DEID_PREP_URL", "http://" + releaseName + "-deid-prep:8080", False)
+            update_parameter(baseURL, contextId, "TERM_SERVICES_PREP_URL", "http://" + releaseName + "-term-services-prep:8080", False)
             update_parameter(baseURL, contextId, "RunASCVD", runASCVD)
             update_parameter(baseURL, contextId, "DeidentifyData", deidentifyData)
             update_parameter(baseURL, contextId, "ResolveTerminology", resolveTerminology)
