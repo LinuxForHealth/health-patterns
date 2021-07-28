910ab650f7e69b8a6e0dac3e52b6c5e9bf5a3b5d
David Ranum <dlranum@us.ibm.com>
2021-07-28 15:16:33 -0500

    review comments

--- services/expose-kafka/exposekafka.py
+++ services/expose-kafka/exposekafka.py
@@ -16,7 +16,18 @@ kafkapw = os.getenv("KAFKAPW")
 kafkabootstrap = os.getenv("KAFKABOOTSTRAP")
 
 # create the kafka topics before use
-initial_topics = os.getenv("INITTOPICS")
+initial_topics = os.getenv("INITTOPICS", "")  #empty list if missing
+numpartitions = os.getenv("INITTOPICNUMPARTITIONS", "1")
+if len(numpartitions) == 0:
+    numpartitions = 1
+else:
+    numpartitions = int(numpartitions)
+
+replication = os.getenv("INITTOPICREPLICATION", "1")
+if len(replication) == 0:
+    replication = 1
+else:
+    replication = int(replication)
 
 init_topics = initial_topics.replace(",", " ")
 topiclist = init_topics.split()
@@ -44,7 +55,7 @@ init_admin_client = KafkaAdminClient(
 new_topic_list = []
 for new_topic in topiclist:
     if new_topic not in existing_topics:
-        new_topic_list.append(NewTopic(name=new_topic, num_partitions=1, replication_factor=1))
+        new_topic_list.append(NewTopic(name=new_topic, num_partitions=numpartitions, replication_factor=replication))
 init_admin_client.create_topics(new_topics=new_topic_list, validate_only=False)
 
 existing_topics = initconsumer.topics() #reset the existing_topics since new ones may have been added
