f77f5f56d806977014a10bc4e050b7acc6866404
Roger Guderian <roger.guderian@ibm.com>
2021-09-15 14:52:45 -0500

    Use all local variables to determine deploy type
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>

--- clinical-enrichment/tests/tests/run-smoketests.sh
+++ clinical-enrichment/tests/tests/run-smoketests.sh
@@ -24,12 +24,14 @@ export TEST_NAMESPACE=$CLUSTER_NAMESPACE"-smoke"
 export INGRESS_SUBDOMAIN=wh-health-patterns.dev.watson-health.ibm.com
 #export JAVA_TOOL_OPTIONS=-Dhttps.protocols=TLSv1.3 
 
-cnd=$CLUSTER_NAMESPACE
+thisdeploy=$CLUSTER_NAMESPACE
+ingest="clinical-ingestion"
+enrich="clinical-enrich"
 
-if [cnd -eq "clinical-enrich"]
+if [$thisdeploy -eq $enrich]
 then
    export HELM_RELEASE=enrich
-elif [cnd -eq "clinical-ingestion"] 
+elif [$thisdeploy -eq $ingest] 
 then
    export HELM_RELEASE=ingestion
 fi
@@ -70,12 +72,12 @@ pwd
 sed -i -e "s/\&hostname replace-me/\&hostname $TEST_NAMESPACE.$INGRESS_SUBDOMAIN/g" values.yaml
 cat values.yaml | grep $TEST_NAMESPACE.$INGRESS_SUBDOMAIN
 
-if [cnd -eq "clinical-enrich"] 
+if [$thisdeploy -eq $enrich] 
 then
    # deploy enrich
    echo "$CLUSTER_NAMESPACE - enrich : Deploy via helm3  using Ingress"
    helm3 install $HELM_RELEASE . -f clinical_enrichment.yaml --set ascvd-from-fhir.ingress.enabled=true --set deid-prep.ingress.enabled=true --set term-services-prep.ingress.enabled=true --set nlp-insights.enabled=true --set nlp-insights.ingress.enabled=true --set nlp-insights.nlpservice.quickumls.endpoint=https://quickumls.wh-health-patterns.dev.watson-health.ibm.com/match --set nlp-insights.nlpservice.acd.endpoint=https://us-east.wh-acd.cloud.ibm.com/wh-acd/api --set nlp-insights.nlpservice.acd.apikey=$ACD_APIKEY --set nlp-insights.nlpservice.acd.flow=wh_acd.ibm_clinical_insights_v1.0_standard_flow --wait --timeout 6m0s
-elif [cnd -eq "clinical-ingestion"] 
+elif [$thisdeploy -eq $ingest] 
 then
    # deploy ingestion
    echo "$CLUSTER_NAMESPACE - ingestion : Deploy via helm3  using Ingress"
@@ -149,7 +151,7 @@ echo NLP Insights server: $NLP_INSIGHTS_IP$NLP_INSIGHTS_PORT
 echo "*************************************"
 
 # Wait for deployment to be ready
-if [cnd -eq "clinical-enrich"] 
+if [$thisdeploy -eq $enrich] 
 then 
    echo "*************************************"
    echo "* Waiting for 2 minutes             *"
@@ -157,7 +159,7 @@ then
    date
    sleep 120  
    date   
-elif [cnd -eq "clinical-ingestion"] 
+elif [$thisdeploy -eq $ingest] 
 then 
    echo "*************************************"
    echo "* Waiting for 5 minutes             *"
@@ -172,7 +174,7 @@ echo "* A Look At Everything              *"
 echo "*************************************"
 kubectl get all
 
-if [cnd -eq "clinical-enrich"] 
+if [$thisdeploy -eq $enrich] 
 then 
    echo "****************************************************" 
    echo "* Goto the testcase folder in the repo             *"
@@ -212,7 +214,7 @@ then
    echo "<testsuites>" > /workspace/clinical-enrichment/tests/smoketests.xml
    cat target/surefire-reports/*.xml >> /workspace/clinical-enrichment/tests/smoketests.xml
    echo "</testsuites>" >> /workspace/clinical-enrichment/tests/smoketests.xml
-elif [cnd -eq "clinical-ingestion"] 
+elif [$thisdeploy -eq $ingest] 
 then
    echo "****************************************************" 
    echo "* Goto the testcase folder in the repo             *"
