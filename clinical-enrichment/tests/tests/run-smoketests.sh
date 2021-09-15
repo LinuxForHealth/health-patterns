1e3627c179b06f54ee936d1c815f7d4fe938e2e3
Roger Guderian <roger.guderian@ibm.com>
2021-09-15 11:40:01 -0500

    Add Split smoketests into enrich & ingest
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>

--- clinical-enrichment/tests/tests/run-smoketests.sh
+++ clinical-enrichment/tests/tests/run-smoketests.sh
@@ -13,26 +13,23 @@ echo "*************************************"
 echo "* Current Directory                 *"
 echo "*************************************"
 pwd
-  
-#echo "*************************************"
-#echo "* Install OpenJDK 11                *"
-#echo "*************************************"
-#wget --quiet https://download.java.net/openjdk/jdk11/ri/openjdk-11+28_linux-x64_bin.tar.gz
-#tar -xf openjdk-11+28_linux-x64_bin.tar.gz
-#sudo mv jdk-14.0.2 /usr/lib/jvm
-
-#ls /usr/lib/jvm
 
 echo "*************************************"
 echo "* Set Test env variables            *"
 echo "*************************************"
 export DEFAULT_PASSWORD=integrati0n
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0
-export HELM_RELEASE=enrich
 export TEST_NAMESPACE=$CLUSTER_NAMESPACE"-smoke"
 #This is for VPC cluster health-patterns-1
 export INGRESS_SUBDOMAIN=wh-health-patterns.dev.watson-health.ibm.com
 #export JAVA_TOOL_OPTIONS=-Dhttps.protocols=TLSv1.3 
+
+if $CLUSTER_NAMESPACE="clinical-enrichment" then
+   export HELM_RELEASE=enrich
+elif $CLUSTER_NAMESPACE="clinical-ingestion" then
+   export HELM_RELEASE=ingestion
+fi
+
 
 echo "*************************************"
 echo "* setup base directory for test     *"
@@ -65,20 +62,21 @@ helm3 dependency update
 echo " Current Directory:"
 pwd
 
-# figure out the ingress subdomain and save as INGRESS_SUBDOMAIN
-# This is for non-VPC clusters
-#export INGRESS_SUBDOMAIN=$(ibmcloud ks cluster get --cluster integration-k8s-cluster -json | grep hostname)
-#export INGRESS_SUBDOMAIN=${INGRESS_SUBDOMAIN:21}
-#export INGRESS_SUBDOMAIN=${INGRESS_SUBDOMAIN:0:${#INGRESS_SUBDOMAIN}-2}
-
 # Add hostname to values.yaml
 sed -i -e "s/\&hostname replace-me/\&hostname $TEST_NAMESPACE.$INGRESS_SUBDOMAIN/g" values.yaml
 cat values.yaml | grep $TEST_NAMESPACE.$INGRESS_SUBDOMAIN
- 
 
-# deploy 
-echo "Deploy via helm3  using Ingress"
-helm3 install $HELM_RELEASE . -f clinical_enrichment.yaml --set ascvd-from-fhir.ingress.enabled=true --set deid-prep.ingress.enabled=true --set term-services-prep.ingress.enabled=true --set nlp-insights.enabled=true --set nlp-insights.ingress.enabled=true --set nlp-insights.nlpservice.quickumls.endpoint=https://quickumls.wh-health-patterns.dev.watson-health.ibm.com/match --set nlp-insights.nlpservice.acd.endpoint=https://us-east.wh-acd.cloud.ibm.com/wh-acd/api --set nlp-insights.nlpservice.acd.apikey=$ACD_APIKEY --set nlp-insights.nlpservice.acd.flow=wh_acd.ibm_clinical_insights_v1.0_standard_flow --wait --timeout 6m0s
+if $CLUSTER_NAMESPACE="clinical-enrichment" then
+   # deploy enrich
+   echo "$CLUSTER_NAMESPACE - enrich : Deploy via helm3  using Ingress"
+   helm3 install $HELM_RELEASE . -f clinical_enrichment.yaml --set ascvd-from-fhir.ingress.enabled=true --set deid-prep.ingress.enabled=true --set term-services-prep.ingress.enabled=true --set nlp-insights.enabled=true --set nlp-insights.ingress.enabled=true --set nlp-insights.nlpservice.quickumls.endpoint=https://quickumls.wh-health-patterns.dev.watson-health.ibm.com/match --set nlp-insights.nlpservice.acd.endpoint=https://us-east.wh-acd.cloud.ibm.com/wh-acd/api --set nlp-insights.nlpservice.acd.apikey=$ACD_APIKEY --set nlp-insights.nlpservice.acd.flow=wh_acd.ibm_clinical_insights_v1.0_standard_flow --wait --timeout 6m0s
+elif $CLUSTER_NAMESPACE="clinical-ingestion" then
+   # deploy ingestion
+   echo "$CLUSTER_NAMESPACE - ingestion : Deploy via helm3  using Ingress"
+   date
+   helm3 install $HELM_RELEASE . -f clinical_ingestion.yaml --wait --timeout 6m0s
+   date
+fi
 
 # setup all the env vars for input to maven
 # FHIR Using INGRESS
@@ -100,6 +98,20 @@ export DEID_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/deid
 export DEID_PORT=
 echo "*************************************"
 echo DEID server: $DEID_IP$DEID_PORT
+echo "*************************************"
+
+# NIFI - using INGRESS
+export NIFI_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/
+export NIFI_PORT=
+echo "*************************************"
+echo NIFI server: $NIFI_IP$NIFI_PORT
+echo "*************************************"
+
+# EXPOSE KAFKA Service using INGRESS
+export EXP_KAFKA_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/expose-kafka
+export EXP_KAFKA_PORT=
+echo "*************************************"
+echo EXPOSE KAFKA Service: $EXP_KAFKA_IP$EXP_KAFKA_PORTT
 echo "*************************************"
 
 # DEID Prep - using INGRESS
@@ -130,64 +142,104 @@ echo "*************************************"
 echo NLP Insights server: $NLP_INSIGHTS_IP$NLP_INSIGHTS_PORT
 echo "*************************************"
 
-# Wait a bit for Nifi to come up
-# Need to change this to some polling of service status for more reliability 
-echo "*************************************"
-echo "* Waiting for 2 minutes             *"
-echo "*************************************"
-date
-sleep 120  
-date
-
-# KAFKA - using Load Balancer
-export KAFKA_IP=$(kubectl get svc --namespace $TEST_NAMESPACE $HELM_RELEASE-kafka-0-external -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
-export KAFKA_PORT=:$(kubectl get svc $HELM_RELEASE-kafka-0-external -o jsonpath='{.spec.ports[0].port}') 
-echo KAFKA server: $KAFKA_IP$KAFKA_PORT
+# Wait for deployment to be ready
+if $CLUSTER_NAMESPACE="clinical-enrichment" then 
+   echo "*************************************"
+   echo "* Waiting for 2 minutes             *"
+   echo "*************************************"
+   date
+   sleep 120  
+   date
+elif $CLUSTER_NAMESPACE="clinical-ingestion" then 
+   echo "*************************************"
+   echo "* Waiting for 5 minutes             *"
+   echo "*************************************"
+   date
+   sleep 300  
+   date
+fi
 
 echo "*************************************" 
 echo "* A Look At Everything              *"
 echo "*************************************"
 kubectl get all
 
-echo "****************************************************" 
-echo "* Goto the testcase folder in the repo             *"
-echo "****************************************************"
-cd /workspace/$TEST_NAMESPACE/health-patterns/clinical-enrichment/
-pwd
-ls
+if $CLUSTER_NAMESPACE="clinical-enrichment" then 
+   echo "****************************************************" 
+   echo "* Goto the testcase folder in the repo             *"
+   echo "****************************************************"
+   cd /workspace/$TEST_NAMESPACE/health-patterns/$CLUSTER_NAMESPACE/
+   pwd
+   ls
 
-echo "*************************************" 
-echo "* Build the testcases               *"
-echo "*************************************"
-mvn clean install -e -Dip.fhir=$FHIR_IP -Dport.fhir=$FHIR_PORT -Dip.fhir.deid=$FHIR_DEID_IP -Dport.fhir.deid=$FHIR_DEID_PORT -Dip.deid.prep=$DEID_PREP_IP -Dport.deid.prep=$DEID_PREP_PORT -Dip.term.prep=$TERM_PREP_IP -Dport.term.prep=$TERM_PREP_PORT -Dip.ascvd.from.fhir=$ASCVD_FROM_FHIR_IP -Dport.ascvd.from.fhir=$ASCVD_FROM_FHIR_PORT -Dip.nlp.insights=$NLP_INSIGHTS_IP -Dport.nlp.insights=$NLP_INSIGHTS_PORT -Dpw=$DEFAULT_PASSWORD
+   echo "*************************************" 
+   echo "* Build the testcases               *"
+   echo "*************************************"
+   mvn clean install -e -Dip.fhir=$FHIR_IP -Dport.fhir=$FHIR_PORT -Dip.fhir.deid=$FHIR_DEID_IP -Dport.fhir.deid=$FHIR_DEID_PORT -Dip.deid.prep=$DEID_PREP_IP -Dport.deid.prep=$DEID_PREP_PORT -Dip.term.prep=$TERM_PREP_IP -Dport.term.prep=$TERM_PREP_PORT -Dip.ascvd.from.fhir=$ASCVD_FROM_FHIR_IP -Dport.ascvd.from.fhir=$ASCVD_FROM_FHIR_PORT -Dip.nlp.insights=$NLP_INSIGHTS_IP -Dport.nlp.insights=$NLP_INSIGHTS_PORT -Dpw=$DEFAULT_PASSWORD
 
-echo "*************************************" 
-echo "* Properties File:                  *"
-echo "*************************************"
-cat src/test/resources/enrich-flow.properties
+   echo "*************************************" 
+   echo "* Properties File:                  *"
+   echo "*************************************"
+   cat src/test/resources/enrich-flow.properties
 
+   echo "*************************************" 
+   echo "* Execute the testcases             *"
+   echo "*************************************"
+   mvn -e -DskipTests=false -Dtest=EnrichmentInitTests test
+   mvn -e -DskipTests=false -Dtest=BasicEnrichmentTests test
+   mvn -e -DskipTests=false -Dtest=EnrichmentConfigTests test
+   mvn -e -DskipTests=false -Dtest=ASCVDEnrichmentTests test
 
-echo "*************************************" 
-echo "* Execute the testcases             *"
-echo "*************************************"
-mvn -e -DskipTests=false -Dtest=EnrichmentInitTests test
-mvn -e -DskipTests=false -Dtest=BasicEnrichmentTests test
-mvn -e -DskipTests=false -Dtest=EnrichmentConfigTests test
-mvn -e -DskipTests=false -Dtest=ASCVDEnrichmentTests test
+   # JUNIT execution reports available in the below folder
+   ls -lrt target/surefire-reports
+   cat target/surefire-reports/categories.EnrichmentInitTests.txt
+   cat target/surefire-reports/categories.BasicEnrichmentTests.txt
+   cat target/surefire-reports/categories.EnrichmentConfigTests.txt
+   cat target/surefire-reports/categories.ASCVDEnrichmentTests.txt
 
-# JUNIT execution reports available in the below folder
-ls -lrt target/surefire-reports
-cat target/surefire-reports/categories.EnrichmentInitTests.txt
-cat target/surefire-reports/categories.BasicEnrichmentTests.txt
-cat target/surefire-reports/categories.EnrichmentConfigTests.txt
-cat target/surefire-reports/categories.ASCVDEnrichmentTests.txt
+   echo "*************************************" 
+   echo "* Report Test Results to Insights   *"
+   echo "*************************************"
+   echo "<testsuites>" > /workspace/$CLUSTER_NAMESPACE/tests/smoketests.xml
+   cat target/surefire-reports/*.xml >> /workspace/$CLUSTER_NAMESPACE/tests/smoketests.xml
+   echo "</testsuites>" >> /workspace/$CLUSTER_NAMESPACE/tests/smoketests.xml
 
-echo "*************************************" 
-echo "* Report Test Results to Insights   *"
-echo "*************************************"
-echo "<testsuites>" > /workspace/clinical-enrichment/tests/smoketests.xml
-cat target/surefire-reports/*.xml >> /workspace/clinical-enrichment/tests/smoketests.xml
-echo "</testsuites>" >> /workspace/clinical-enrichment/tests/smoketests.xml
+elif $CLUSTER_NAMESPACE="clinical-ingestion" then
+   echo "****************************************************" 
+   echo "* Goto the testcase folder in the repo             *"
+   echo "****************************************************"
+   cd /workspace/$TEST_NAMESPACE/health-patterns/ingest/
+   pwd
+   ls
+   
+   echo "*************************************" 
+   echo "* Build the testcases               *"
+   echo "*************************************"
+   mvn clean install -e -Dip.fhir=$FHIR_IP -Dport.fhir=$FHIR_PORT -Dip.fhir.deid=$FHIR_DEID_IP -Dport.fhir.deid=$FHIR_DEID_PORT -Dip.nifi=$NIFI_IP -Dport.nifi=$NIFI_PORT -Dip.nifi.api=$NIFI_API_IP -Dport.nifi.api=$NIFI_API_PORT -Dip.kafka=$KAFKA_IP -Dport.kafka=$KAFKA_PORT -Dip.deid=$DEID_IP -Dport.deid=$DEID_PORT -Dip.expkafka=$EXP_KAFKA_IP -Dport.expkafka=$EXP_KAFKA_PORT -Dpw=$DEFAULT_PASSWORD
+
+   echo "*************************************" 
+   echo "* Initialize the testcases          *"
+   echo "*************************************"
+   mvn -e -DskipTests=false -Dtest=BasicClinicalIngestionInitTests test
+
+   echo "*************************************" 
+   echo "* Execute the testcases             *"
+   echo "*************************************"
+   mvn -e -DskipTests=false -Dtest=BasicClinicalIngestionFlowTests test 
+
+   # JUNIT execution reports available in the below folder
+   ls -lrt target/surefire-reports
+   cat target/surefire-reports/categories.BasicClinicalIngestionInitTests.txt
+   cat target/surefire-reports/categories.BasicClinicalIngestionFlowTests.txt
+
+   echo "*************************************" 
+   echo "* Report Test Results to Insights   *"
+   echo "*************************************"
+   echo "<testsuites>" > /workspace/ingest/tests/smoketests.xml
+   cat target/surefire-reports/*.xml >> /workspace/ingest/tests/smoketests.xml
+   echo "</testsuites>" >> /workspace/ingestion/tests/smoketests.xml
+fi
+
 
 # then clean up
 echo "*************************************"
