4a4117fed7ba0656575931bc8f789910131a2625
Roger Guderian <roger.guderian@ibm.com>
2021-09-15 13:22:24 -0500

    More if-then-else syntax corrections
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>

--- clinical-enrichment/tests/tests/run-smoketests.sh
+++ clinical-enrichment/tests/tests/run-smoketests.sh
@@ -26,7 +26,8 @@ export INGRESS_SUBDOMAIN=wh-health-patterns.dev.watson-health.ibm.com
 
 if [$CLUSTER_NAMESPACE = "clinical-enrich"] then
    export HELM_RELEASE=enrich
-elif [$CLUSTER_NAMESPACE = "clinical-ingestion"] then
+fi
+else if [$CLUSTER_NAMESPACE = "clinical-ingestion"] then
    export HELM_RELEASE=ingestion
 fi
 
@@ -70,7 +71,8 @@ if [$CLUSTER_NAMESPACE = "clinical-enrich"] then
    # deploy enrich
    echo "$CLUSTER_NAMESPACE - enrich : Deploy via helm3  using Ingress"
    helm3 install $HELM_RELEASE . -f clinical_enrichment.yaml --set ascvd-from-fhir.ingress.enabled=true --set deid-prep.ingress.enabled=true --set term-services-prep.ingress.enabled=true --set nlp-insights.enabled=true --set nlp-insights.ingress.enabled=true --set nlp-insights.nlpservice.quickumls.endpoint=https://quickumls.wh-health-patterns.dev.watson-health.ibm.com/match --set nlp-insights.nlpservice.acd.endpoint=https://us-east.wh-acd.cloud.ibm.com/wh-acd/api --set nlp-insights.nlpservice.acd.apikey=$ACD_APIKEY --set nlp-insights.nlpservice.acd.flow=wh_acd.ibm_clinical_insights_v1.0_standard_flow --wait --timeout 6m0s
-elif [$CLUSTER_NAMESPACE = "clinical-ingestion"] then
+fi
+else if [$CLUSTER_NAMESPACE = "clinical-ingestion"] then
    # deploy ingestion
    echo "$CLUSTER_NAMESPACE - ingestion : Deploy via helm3  using Ingress"
    date
@@ -150,7 +152,8 @@ if [$CLUSTER_NAMESPACE = "clinical-enrich"] then
    date
    sleep 120  
    date
-elif [$CLUSTER_NAMESPACE = "clinical-ingestion"] then 
+fi   
+else if [$CLUSTER_NAMESPACE = "clinical-ingestion"] then 
    echo "*************************************"
    echo "* Waiting for 5 minutes             *"
    echo "*************************************"
@@ -203,8 +206,8 @@ if [$CLUSTER_NAMESPACE = "clinical-enrich"] then
    echo "<testsuites>" > /workspace/clinical-enrichment/tests/smoketests.xml
    cat target/surefire-reports/*.xml >> /workspace/clinical-enrichment/tests/smoketests.xml
    echo "</testsuites>" >> /workspace/clinical-enrichment/tests/smoketests.xml
-
-elif [$CLUSTER_NAMESPACE = "clinical-ingestion"] then
+fi
+else if [$CLUSTER_NAMESPACE = "clinical-ingestion"] then
    echo "****************************************************" 
    echo "* Goto the testcase folder in the repo             *"
    echo "****************************************************"
