3a20858bd87ed7d0e9adb3ce96b0f494e224db22
Roger Guderian <roger.guderian@ibm.com>
2021-09-16 09:19:13 -0500

    More work on reporting ingest test results
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>

--- clinical-enrichment/tests/tests/run-smoketests.sh
+++ clinical-enrichment/tests/tests/run-smoketests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Run the Avlearie Clinical Enrichment Deploy install per documented instructions
+# Run the Avlearie enrich/ingestion Deploy install per documented instructions (selection based on CLUSER_NAMESPACE from toolchain input
 #
-# Run the Clinical Enrichment smoke tests and add test results to smoketests.xml for the Insights Quality Dashboard
+# Run the enrich/ingestion smoke tests and add test results to smoketests.xml for the Insights Quality Dashboard
 
 echo "*************************************"
 echo "* Linux version                     *"
@@ -20,22 +20,25 @@ echo "*************************************"
 export DEFAULT_PASSWORD=integrati0n
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0
 export TEST_NAMESPACE=$CLUSTER_NAMESPACE"-smoke"
-#This is for VPC cluster health-patterns-1
-export INGRESS_SUBDOMAIN=wh-health-patterns.dev.watson-health.ibm.com
-#export JAVA_TOOL_OPTIONS=-Dhttps.protocols=TLSv1.3 
 
+#This is for VPC cluster health-patterns-1
+export INGRESS_SUBDOMAIN=wh-health-patterns.dev.watson-health.ibm.com 
+
+# Setting these variables to make if-then-else logic readable/understandable
 thisdeploy=$CLUSTER_NAMESPACE
 ingest="clinical-ingestion"
 enrich="clinical-enrich"
 
+# deploymemt-specific variables/values
 if [ $thisdeploy = $enrich ]
 then
    export HELM_RELEASE=enrich
+   deploywait=120
 elif [ $thisdeploy = $ingest ] 
 then
    export HELM_RELEASE=ingestion
+   deploywait=300
 fi
-
 
 echo "*************************************"
 echo "* setup base directory for test     *"
@@ -72,15 +75,16 @@ pwd
 sed -i -e "s/\&hostname replace-me/\&hostname $TEST_NAMESPACE.$INGRESS_SUBDOMAIN/g" values.yaml
 cat values.yaml | grep $TEST_NAMESPACE.$INGRESS_SUBDOMAIN
 
+# Execute the desired deployment
 if [ $thisdeploy = $enrich ] 
 then
    # deploy enrich
-   echo "$CLUSTER_NAMESPACE - enrich : Deploy via helm3  using Ingress"
+   echo "$CLUSTER_NAMESPACE - enrich : Deploy via helm3"
    helm3 install $HELM_RELEASE . -f clinical_enrichment.yaml --set ascvd-from-fhir.ingress.enabled=true --set deid-prep.ingress.enabled=true --set term-services-prep.ingress.enabled=true --set nlp-insights.enabled=true --set nlp-insights.ingress.enabled=true --set nlp-insights.nlpservice.quickumls.endpoint=https://quickumls.wh-health-patterns.dev.watson-health.ibm.com/match --set nlp-insights.nlpservice.acd.endpoint=https://us-east.wh-acd.cloud.ibm.com/wh-acd/api --set nlp-insights.nlpservice.acd.apikey=$ACD_APIKEY --set nlp-insights.nlpservice.acd.flow=wh_acd.ibm_clinical_insights_v1.0_standard_flow --wait --timeout 6m0s
 elif [ $thisdeploy = $ingest ] 
 then
    # deploy ingestion
-   echo "$CLUSTER_NAMESPACE - ingestion : Deploy via helm3  using Ingress"
+   echo "$CLUSTER_NAMESPACE - ingestion : Deploy via helm3"
    date
    helm3 install $HELM_RELEASE . -f clinical_ingestion.yaml --wait --timeout 6m0s
    date
@@ -150,24 +154,13 @@ echo "*************************************"
 echo NLP Insights server: $NLP_INSIGHTS_IP$NLP_INSIGHTS_PORT
 echo "*************************************"
 
-# Wait for deployment to be ready
-if [ $thisdeploy = $enrich ] 
-then 
-   echo "*************************************"
-   echo "* Waiting for 2 minutes             *"
-   echo "*************************************"
-   date
-   sleep 120  
-   date   
-elif [ $thisdeploy = $ingest ] 
-then 
-   echo "*************************************"
-   echo "* Waiting for 5 minutes             *"
-   echo "*************************************"
-   date
-   sleep 300  
-   date
-fi
+
+echo "*************************************"
+echo "* Waiting for "$deploywait" seconds             *"
+echo "*************************************"
+date
+sleep $deploywait  
+date
 
 echo "*************************************" 
 echo "* A Look At Everything              *"
@@ -189,11 +182,6 @@ then
    mvn clean install -e -Dip.fhir=$FHIR_IP -Dport.fhir=$FHIR_PORT -Dip.fhir.deid=$FHIR_DEID_IP -Dport.fhir.deid=$FHIR_DEID_PORT -Dip.deid.prep=$DEID_PREP_IP -Dport.deid.prep=$DEID_PREP_PORT -Dip.term.prep=$TERM_PREP_IP -Dport.term.prep=$TERM_PREP_PORT -Dip.ascvd.from.fhir=$ASCVD_FROM_FHIR_IP -Dport.ascvd.from.fhir=$ASCVD_FROM_FHIR_PORT -Dip.nlp.insights=$NLP_INSIGHTS_IP -Dport.nlp.insights=$NLP_INSIGHTS_PORT -Dpw=$DEFAULT_PASSWORD
 
    echo "*************************************" 
-   echo "* Properties File:                  *"
-   echo "*************************************"
-   cat src/test/resources/enrich-flow.properties
-
-   echo "*************************************" 
    echo "* Execute the testcases             *"
    echo "*************************************"
    mvn -e -DskipTests=false -Dtest=EnrichmentInitTests test
@@ -207,13 +195,6 @@ then
    cat target/surefire-reports/categories.BasicEnrichmentTests.txt
    cat target/surefire-reports/categories.EnrichmentConfigTests.txt
    cat target/surefire-reports/categories.ASCVDEnrichmentTests.txt
-
-   echo "*************************************" 
-   echo "* Report Test Results to Insights   *"
-   echo "*************************************"
-   echo "<testsuites>" > /workspace/clinical-enrichment/tests/smoketests.xml
-   cat target/surefire-reports/*.xml >> /workspace/clinical-enrichment/tests/smoketests.xml
-   echo "</testsuites>" >> /workspace/clinical-enrichment/tests/smoketests.xml
 elif [ $thisdeploy = $ingest ] 
 then
    echo "****************************************************" 
@@ -229,7 +210,7 @@ then
    mvn clean install -e -Dip.fhir=$FHIR_IP -Dport.fhir=$FHIR_PORT -Dip.fhir.deid=$FHIR_DEID_IP -Dport.fhir.deid=$FHIR_DEID_PORT -Dip.nifi=$NIFI_IP -Dport.nifi=$NIFI_PORT -Dip.nifi.api=$NIFI_API_IP -Dport.nifi.api=$NIFI_API_PORT -Dip.kafka=$KAFKA_IP -Dport.kafka=$KAFKA_PORT -Dip.deid=$DEID_IP -Dport.deid=$DEID_PORT -Dip.expkafka=$EXP_KAFKA_IP -Dport.expkafka=$EXP_KAFKA_PORT -Dpw=$DEFAULT_PASSWORD
 
    echo "*************************************" 
-   echo "* Initialize the testcases          *"
+   echo "* Execute Initialize testcases      *"
    echo "*************************************"
    mvn -e -DskipTests=false -Dtest=BasicIngestionInitTests test
 
@@ -242,15 +223,14 @@ then
    ls -lrt target/surefire-reports
    cat target/surefire-reports/categories.BasicIngestionInitTests.txt
    cat target/surefire-reports/categories.BasicIngestionTests.txt
-
-   echo "*************************************" 
-   echo "* Report Test Results to Insights   *"
-   echo "*************************************"
-   echo "<testsuites>" > /workspace/clinical-enrichment/tests/smoketests.xml
-   cat target/surefire-reports/*.xml >> /workspace/clinical-enrichment/tests/smoketests.xml
-   echo "</testsuites>" >> /workspace/clinical-enrichment/smoketests.xml
 fi
 
+echo "*************************************" 
+echo "* Report Test Results to Insights   *"
+echo "*************************************"
+echo "<testsuites>" > /workspace/clinical-enrichment/tests/smoketests.xml
+cat target/surefire-reports/*.xml >> /workspace/clinical-enrichment/tests/smoketests.xml
+echo "</testsuites>" >> /workspace/clinical-enrichment/tests/smoketests.xml
 
 # then clean up
 echo "*************************************"
@@ -259,19 +239,3 @@ echo "*************************************"
 helm3 delete $HELM_RELEASE
 kubectl delete namespace $TEST_NAMESPACE
 
-
-# temp create a results file with "good" results while this script is developed
-#cd /workspace/clinical-ingestion/e2e-tests
-#output="<testcase classname=\"bash\" name=\"test1\" time=\"0\"/>"
-#currentTime=`date +"%Y-%m-%dT%T"`
-#header="<testsuite name=\"Smoke tests\" tests=\"0\" failures=\"0\" errors=\"0\" skipped=\"0\" timestamp=\"${currentTime}\" time=\"0\">"
-#footer="</testsuite>"
-
-#echo "Current Directory"
-#pwd
-
-#cat << EOF > smoketests.xml
-#$header
-#$output
-#$footer
-#EOF
