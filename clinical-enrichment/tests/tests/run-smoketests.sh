1b18de6602b016ac0f6b02b09504839cae37a5db
Roger Guderian <roger.guderian@ibm.com>
2021-09-16 11:01:45 -0500

    Add Common processing script
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>

--- clinical-enrichment/tests/tests/run-smoketests.sh
+++ clinical-enrichment/tests/tests/run-smoketests.sh
@@ -3,156 +3,24 @@
 # Run the Avlearie enrich/ingestion Deploy install per documented instructions (selection based on CLUSER_NAMESPACE from toolchain input
 #
 # Run the enrich/ingestion smoke tests and add test results to smoketests.xml for the Insights Quality Dashboard
-
-echo "*************************************"
-echo "* Linux version                     *"
-echo "*************************************"
-cat /etc/redhat-release
-
-echo "*************************************"
-echo "* Current Directory                 *"
-echo "*************************************"
-pwd
-
-echo "*************************************"
-echo "* Set Test env variables            *"
-echo "*************************************"
-export DEFAULT_PASSWORD=integrati0n
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0
-export TEST_NAMESPACE=$CLUSTER_NAMESPACE"-smoke"
-
-#This is for VPC cluster health-patterns-1
-export INGRESS_SUBDOMAIN=wh-health-patterns.dev.watson-health.ibm.com 
-
-# Setting these variables to make if-then-else logic readable/understandable
-thisdeploy=$CLUSTER_NAMESPACE
-ingest="clinical-ingestion"
-enrich="clinical-enrich"
-
-# deploymemt-specific variables/values
-if [ $thisdeploy = $enrich ]
-then
-   export HELM_RELEASE=enrich
-   deploywait=120
-elif [ $thisdeploy = $ingest ] 
-then
-   export HELM_RELEASE=ingestion
-   deploywait=300
-fi
-
-echo "*************************************"
-echo "* setup base directory for test     *"
-echo "*************************************"
-cd /workspace
-mkdir $TEST_NAMESPACE
-cd $TEST_NAMESPACE
-
-echo "*************************************"
-echo "* create namespace and set to it    *"
-echo "*************************************"
-kubectl create namespace $TEST_NAMESPACE
-
-echo " Set namespace to $TEST_NAMESPACE"
-kubectl config set-context --current --namespace=$TEST_NAMESPACE
-
-echo "*************************************"
-echo "* Clinical Ingestion Deploy         *"
-echo "*************************************"
-
-echo " clone clinical ingestion git repo using "$GIT_BRANCH " branch"
-git clone --branch $GIT_BRANCH https://github.com/Alvearie/health-patterns.git
+ 
+chmod +x toolchain_envsetup.sh
+source ./toolchain_envsetup.sh "-smoke"
 
 echo " change to the correct directory"
-cd health-patterns/helm-charts/health-patterns
-
-echo " helm dependency update"
-helm3 dependency update
-
-echo " Current Directory:"
-pwd
-
-# Add hostname to values.yaml
-sed -i -e "s/\&hostname replace-me/\&hostname $TEST_NAMESPACE.$INGRESS_SUBDOMAIN/g" values.yaml
-cat values.yaml | grep $TEST_NAMESPACE.$INGRESS_SUBDOMAIN
+cd /workspace/$TEST_NAMESPACE/health-patterns/helm-charts/health-patterns
 
 # Execute the desired deployment
-if [ $thisdeploy = $enrich ] 
+echo $CLUSTER_NAMESPACE" : Deploy via helm3"
+if [ $CLUSER_NAMESPACE = "clinical-enrich" ] 
 then
    # deploy enrich
-   echo "$CLUSTER_NAMESPACE - enrich : Deploy via helm3"
    helm3 install $HELM_RELEASE . -f clinical_enrichment.yaml --set ascvd-from-fhir.ingress.enabled=true --set deid-prep.ingress.enabled=true --set term-services-prep.ingress.enabled=true --set nlp-insights.enabled=true --set nlp-insights.ingress.enabled=true --set nlp-insights.nlpservice.quickumls.endpoint=https://quickumls.wh-health-patterns.dev.watson-health.ibm.com/match --set nlp-insights.nlpservice.acd.endpoint=https://us-east.wh-acd.cloud.ibm.com/wh-acd/api --set nlp-insights.nlpservice.acd.apikey=$ACD_APIKEY --set nlp-insights.nlpservice.acd.flow=wh_acd.ibm_clinical_insights_v1.0_standard_flow --wait --timeout 6m0s
-elif [ $thisdeploy = $ingest ] 
+elif [ $CLUSER_NAMESPACE = "clinical-ingestion" ] 
 then
    # deploy ingestion
-   echo "$CLUSTER_NAMESPACE - ingestion : Deploy via helm3"
-   date
    helm3 install $HELM_RELEASE . -f clinical_ingestion.yaml --wait --timeout 6m0s
-   date
 fi
-
-# setup all the env vars for input to maven
-# FHIR Using INGRESS
-export FHIR_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/fhir
-export FHIR_PORT=
-echo "*************************************"
-echo FHIR server: $FHIR_IP$FHIR_PORT
-echo "*************************************"
-
-# FHIR DEID - using INGRESS
-export FHIR_DEID_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/fhir-deid
-export FHIR_DEID_PORT=
-echo "*************************************"
-echo FHIR DEID server: $FHIR_DEID_IP$FHIR_DEID_PORT
-echo "*************************************"
-
-# DEID - using INGRESS
-export DEID_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/deid
-export DEID_PORT=
-echo "*************************************"
-echo DEID server: $DEID_IP$DEID_PORT
-echo "*************************************"
-
-# NIFI - using INGRESS
-export NIFI_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/
-export NIFI_PORT=
-echo "*************************************"
-echo NIFI server: $NIFI_IP$NIFI_PORT
-echo "*************************************"
-
-# EXPOSE KAFKA Service using INGRESS
-export EXP_KAFKA_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/expose-kafka
-export EXP_KAFKA_PORT=
-echo "*************************************"
-echo EXPOSE KAFKA Service: $EXP_KAFKA_IP$EXP_KAFKA_PORTT
-echo "*************************************"
-
-# DEID Prep - using INGRESS
-export DEID_PREP_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/deid-prep
-export DEID_PREP_PORT=
-echo "*************************************"
-echo DEID-PREP server: $DEID_PREP_IP$DEID_PREP_PORT
-echo "*************************************"
-
-# TERM Services Prep - using INGRESS
-export TERM_PREP_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/term-services-prep
-export TERM_PREP_PORT=
-echo "*************************************"
-echo TERM-SERVICES-PREP server: $TERM_PREP_IP$TERM_PREP_PORT
-echo "*************************************"
-
-# ASCVD From FHIR Service - using INGRESS
-export ASCVD_FROM_FHIR_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/ascvd-from-fhir
-export ASCVD_FROM_FHIR_PORT=
-echo "*************************************"
-echo ASCVD-FROM-FHIR server: $ASCVD_FROM_FHIR_IP$ASCVD_FROM_FHIR_PORT
-echo "*************************************"
-
-# NLP Insights Service - using INGRESS
-export NLP_INSIGHTS_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/nlp-insights
-export NLP_INSIGHTS_PORT=
-echo "*************************************"
-echo NLP Insights server: $NLP_INSIGHTS_IP$NLP_INSIGHTS_PORT
-echo "*************************************"
 
 
 echo "*************************************"
