1fe9ed0c3feace90bb429eba23fb4afab072df85
Roger Guderian <roger.guderian@ibm.com>
2021-09-16 11:55:01 -0500

    fixed name of setup script _ vs -
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>

--- clinical-enrichment/tests/tests/run-smoketests.sh
+++ clinical-enrichment/tests/tests/run-smoketests.sh
@@ -3,14 +3,9 @@
 # Run the Avlearie enrich/ingestion Deploy install per documented instructions (selection based on CLUSER_NAMESPACE from toolchain input
 #
 # Run the enrich/ingestion smoke tests and add test results to smoketests.xml for the Insights Quality Dashboard
-echo "*************************************" 
-echo "* Where are we located?             *"
-echo "*************************************"
-pwd
-cd tests
-ls
-chmod +x toolchain_envsetup.sh
-source ./toolchain_envsetup.sh "-smoke"
+
+chmod +x ./tests/toolchain-envsetup.sh
+source ./tests/toolchain-envsetup.sh "-smoke"
 
 echo " change to the correct directory"
 cd /workspace/$TEST_NAMESPACE/health-patterns/helm-charts/health-patterns
