6bc961febc55bf48fc6492e42363ac121a36116b
Roger Guderian <72989284+rguderian@users.noreply.github.com>
2021-09-17 11:04:38 -0500

    Add ingest tests to health-patterns repo toolchain (#247)
    * Add Split smoketests into enrich & ingest
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Change base namespace from clinical-enrichment to clinical-enrich
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Fix conditional statement syntax
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Another Syntax Correction
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * More if-then-else syntax corrections
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Another attempt at correct if-then-else syntax
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Cluster namespace debug code
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Removed Debug Code - another try at correct condition syntax
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Use == instead of =
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Use local var set the CLUSTER_NAMESPACE env var value
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * try -eq instead of ==
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Another variation
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * try #!/bin/bash
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Using local variable
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Another attempt....
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Use all local variables to determine deploy type
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * use = in place of -eq
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Add spaces before and after square brackets
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Use correct ingest test categories
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Fix filepath to test results
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Fix more test result filepaths
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Another fix to the test results reporting
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * More work on reporting ingest test results
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Simplified if-then-else logic
    More work on ingest test results reporting to Insights
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Add Common processing script
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Fixed location of toolchain-envsetup.sh
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Add debug code
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Another Fix for toolchain-envsetup location
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * different method to access setup script
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Starting location debug code
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * fixed name of setup script _ vs -
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Fix a bunch of typos
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Add ingest tests to fvt and ivt
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Fix results reporting for fvt & ivt for ingest tests
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Add FHIR Proxy Ingress Env Vars
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>
    
    * Small bits of code cleanup
    
    Signed-off-by: Roger Guderian <roger.guderian@ibm.com>

--- clinical-enrichment/tests/tests/run-ivttests.sh
+++ clinical-enrichment/tests/tests/run-ivttests.sh
@@ -1,187 +1,107 @@
 #!/usr/bin/env bash
 
-# Run the Avlearie Clinical Enrichment Deploy install per documented instructions
+# Run the Avlearie enrich/ingestion Deploy install per documented instructions (selection based on CLUSER_NAMESPACE from toolchain input
 #
-# Run the Clinical Enrichment smoke tests and add test results to ivttest.xml for the Insights Quality Dashboard
+# Run the enrich/ingestion smoke tests and add test results to ivttests.xml for the Insights Quality Dashboard
+
+# Setup the test environment
+chmod +x ./tests/toolchain-envsetup.sh
+source ./tests/toolchain-envsetup.sh "ivt"
+
+echo " change to the correct deployment directory"
+cd /workspace/$TEST_NAMESPACE/health-patterns/helm-charts/health-patterns
+
+# Execute the desired deployment
+echo $TEST_NAMESPACE" : Deploy via helm3"
+if [ $CLUSTER_NAMESPACE = "clinical-enrich" ] 
+then
+  # deploy enrich
+  helm3 install $HELM_RELEASE . -f clinical_enrichment.yaml --set ascvd-from-fhir.ingress.enabled=true --set deid-prep.ingress.enabled=true --set term-services-prep.ingress.enabled=true --set nlp-insights.enabled=true --set nlp-insights.ingress.enabled=true --set nlp-insights.nlpservice.quickumls.endpoint=https://quickumls.wh-health-patterns.dev.watson-health.ibm.com/match --set nlp-insights.nlpservice.acd.endpoint=https://us-east.wh-acd.cloud.ibm.com/wh-acd/api --set nlp-insights.nlpservice.acd.apikey=$ACD_APIKEY --set nlp-insights.nlpservice.acd.flow=wh_acd.ibm_clinical_insights_v1.0_standard_flow --wait --timeout 6m0s
+elif [ $CLUSTER_NAMESPACE = "clinical-ingestion" ] 
+then
+   # deploy ingestion
+   helm3 install $HELM_RELEASE . -f de-id-pattern-values.yaml -f clinical_ingestion.yaml --set fhir.proxy.enabled=true --set fhir-deid.proxy.enabled=true --set nlp-insights.enabled=true --set nlp-insights.nlpservice.quickumls.endpoint=https://quickumls.wh-health-patterns.dev.watson-health.ibm.com/match --wait --timeout 6m0s
+fi
+
 
 echo "*************************************"
-echo "* Linux version                     *"
-echo "*************************************"
-cat /etc/redhat-release
-
-echo "*************************************"
-echo "* Current Directory                 *"
-echo "*************************************"
-pwd
-  
-#echo "*************************************"
-#echo "* Install OpenJDK 11                *"
-#echo "*************************************"
-#wget --quiet https://download.java.net/openjdk/jdk11/ri/openjdk-11+28_linux-x64_bin.tar.gz
-#tar -xf openjdk-11+28_linux-x64_bin.tar.gz
-#sudo mv jdk-14.0.2 /usr/lib/jvm
-
-#ls /usr/lib/jvm
-
-echo "*************************************"
-echo "* Set Test env variables            *"
-echo "*************************************"
-export DEFAULT_PASSWORD=integrati0n
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0
-export HELM_RELEASE=enrich
-export TEST_NAMESPACE=$CLUSTER_NAMESPACE"-ivt"
-#This is for VPC cluster health-patterns-1
-export INGRESS_SUBDOMAIN=wh-health-patterns.dev.watson-health.ibm.com
-#export JAVA_TOOL_OPTIONS=-Dhttps.protocols=TLSv1.3 
-
-echo "*************************************"
-echo "* setup base directory for test     *"
-echo "*************************************"
-cd /workspace
-mkdir $TEST_NAMESPACE
-cd $TEST_NAMESPACE
-
-echo "*************************************"
-echo "* create namespace and set to it    *"
-echo "*************************************"
-kubectl create namespace $TEST_NAMESPACE
-
-echo " Set namespace to $TEST_NAMESPACE"
-kubectl config set-context --current --namespace=$TEST_NAMESPACE
-
-echo "*************************************"
-echo "* Clinical Ingestion Deploy         *"
-echo "*************************************"
-
-echo " clone clinical ingestion git repo using "$GIT_BRANCH " branch"
-git clone --branch $GIT_BRANCH https://github.com/Alvearie/health-patterns.git
-
-echo " change to the correct directory"
-cd health-patterns/helm-charts/health-patterns
-
-echo " helm dependency update"
-helm3 dependency update
-
-echo " Current Directory:"
-pwd
-
-# figure out the ingress subdomain and save as INGRESS_SUBDOMAIN
-# This is for non-VPC clusters
-#export INGRESS_SUBDOMAIN=$(ibmcloud ks cluster get --cluster integration-k8s-cluster -json | grep hostname)
-#export INGRESS_SUBDOMAIN=${INGRESS_SUBDOMAIN:21}
-#export INGRESS_SUBDOMAIN=${INGRESS_SUBDOMAIN:0:${#INGRESS_SUBDOMAIN}-2}
-
-# Add hostname to values.yaml
-sed -i -e "s/\&hostname replace-me/\&hostname $TEST_NAMESPACE.$INGRESS_SUBDOMAIN/g" values.yaml
-cat values.yaml | grep $TEST_NAMESPACE.$INGRESS_SUBDOMAIN
-  
-# deploy 
-echo "Deploy via helm3  using Ingress"
-helm3 install $HELM_RELEASE . -f clinical_enrichment.yaml --set ascvd-from-fhir.ingress.enabled=true --set deid-prep.ingress.enabled=true --set term-services-prep.ingress.enabled=true --set nlp-insights.enabled=true --set nlp-insights.ingress.enabled=true --set nlp-insights.nlpservice.quickumls.endpoint=https://quickumls.wh-health-patterns.dev.watson-health.ibm.com/match --set nlp-insights.nlpservice.acd.endpoint=https://us-east.wh-acd.cloud.ibm.com/wh-acd/api --set nlp-insights.nlpservice.acd.apikey=$ACD_APIKEY --set nlp-insights.nlpservice.acd.flow=wh_acd.ibm_clinical_insights_v1.0_standard_flow --wait --timeout 6m0s
-
-# setup all the env vars for input to maven
-# FHIR Using INGRESS
-export FHIR_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/fhir
-export FHIR_PORT=
-echo "*************************************"
-echo FHIR server: $FHIR_IP$FHIR_PORT
-echo "*************************************"
-
-# FHIR DEID - using INGRESS
-export FHIR_DEID_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/fhir-deid
-export FHIR_DEID_PORT=
-echo "*************************************"
-echo FHIR DEID server: $FHIR_DEID_IP$FHIR_DEID_PORT
-echo "*************************************"
-
-# DEID - using INGRESS
-export DEID_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/deid
-export DEID_PORT=
-echo "*************************************"
-echo DEID server: $DEID_IP$DEID_PORT
-echo "*************************************"
-
-# DEID Prep - using INGRESS
-export DEID_PREP_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/deid-prep
-export DEID_PREP_PORT=
-echo "*************************************"
-echo DEID-PREP server: $DEID_PREP_IP$DEID_PREP_PORT
-echo "*************************************"
-
-# TERM Services Prep - using INGRESS
-export TERM_PREP_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/term-services-prep
-export TERM_PREP_PORT=
-echo "*************************************"
-echo TERM-SERVICES-PREP server: $TERM_PREP_IP$TERM_PREP_PORT
-echo "*************************************"
-
-# ASCVD From FHIR Service - using INGRESS
-export ASCVD_FROM_FHIR_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/ascvd-from-fhir
-export ASCVD_FROM_FHIR_PORT=
-echo "*************************************"
-echo ASCVD-FROM-FHIR server: $ASCVD_FROM_FHIR_IP$ASCVD_FROM_FHIR_PORT
-echo "*************************************"
-
-# NLP Insights Service - using INGRESS
-export NLP_INSIGHTS_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/nlp-insights
-export NLP_INSIGHTS_PORT=
-echo "*************************************"
-echo NLP Insights server: $NLP_INSIGHTS_IP$NLP_INSIGHTS_PORT
-echo "*************************************"
-
-# Wait a bit for Nifi to come up
-# Need to change this to some polling of service status for more reliability 
-echo "*************************************"
-echo "* Waiting for 2 minutes             *"
+echo "* Waiting for "$deploywait" seconds           *"
 echo "*************************************"
 date
-sleep 120  
+sleep $deploywait  
 date
-
-# KAFKA - using Load Balancer
-export KAFKA_IP=$(kubectl get svc --namespace $TEST_NAMESPACE $HELM_RELEASE-kafka-0-external -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
-export KAFKA_PORT=:$(kubectl get svc $HELM_RELEASE-kafka-0-external -o jsonpath='{.spec.ports[0].port}') 
-echo KAFKA server: $KAFKA_IP$KAFKA_PORT
 
 echo "*************************************" 
 echo "* A Look At Everything              *"
 echo "*************************************"
 kubectl get all
 
-echo "****************************************************" 
-echo "* Goto the testcase folder in the repo             *"
-echo "****************************************************"
-cd /workspace/$TEST_NAMESPACE/health-patterns/clinical-enrichment/
-pwd
-ls
 
-echo "*************************************" 
-echo "* Build the testcases               *"
-echo "*************************************"
-mvn clean install -e -Dip.fhir=$FHIR_IP -Dport.fhir=$FHIR_PORT -Dip.fhir.deid=$FHIR_DEID_IP -Dport.fhir.deid=$FHIR_DEID_PORT -Dip.deid.prep=$DEID_PREP_IP -Dport.deid.prep=$DEID_PREP_PORT -Dip.term.prep=$TERM_PREP_IP -Dport.term.prep=$TERM_PREP_PORT -Dip.ascvd.from.fhir=$ASCVD_FROM_FHIR_IP -Dport.ascvd.from.fhir=$ASCVD_FROM_FHIR_PORT -Dip.nlp.insights=$NLP_INSIGHTS_IP -Dport.nlp.insights=$NLP_INSIGHTS_PORT -Dpw=$DEFAULT_PASSWORD
+if [ $CLUSTER_NAMESPACE = "clinical-enrich" ]  
+then
 
-echo "*************************************" 
-echo "* Properties File:                  *"
-echo "*************************************"
-cat src/test/resources/enrich-flow.properties
+   echo "****************************************************" 
+   echo "* Goto the testcase folder in the repo             *"
+   echo "****************************************************"
+   cd /workspace/$TEST_NAMESPACE/health-patterns/clinical-enrichment/
 
+   echo "*************************************" 
+   echo "* Build the testcases               *"
+   echo "*************************************"
+   mvn clean install -e -Dip.fhir=$FHIR_IP -Dport.fhir=$FHIR_PORT -Dip.fhir.deid=$FHIR_DEID_IP -Dport.fhir.deid=$FHIR_DEID_PORT -Dip.deid.prep=$DEID_PREP_IP -Dport.deid.prep=$DEID_PREP_PORT -Dip.term.prep=$TERM_PREP_IP -Dport.term.prep=$TERM_PREP_PORT -Dip.ascvd.from.fhir=$ASCVD_FROM_FHIR_IP -Dport.ascvd.from.fhir=$ASCVD_FROM_FHIR_PORT -Dip.nlp.insights=$NLP_INSIGHTS_IP -Dport.nlp.insights=$NLP_INSIGHTS_PORT -Dpw=$DEFAULT_PASSWORD
 
+   echo "*************************************" 
+   echo "* Execute the testcases             *"
+   echo "*************************************"
+   mvn -e -DskipTests=false -Dtest=EnrichmentInitTests test
+   mvn -e -DskipTests=false -Dtest=BasicEnrichmentTests test
+   mvn -e -DskipTests=false -Dtest=EnrichmentConfigTests test
+   mvn -e -DskipTests=false -Dtest=ASCVDEnrichmentTests test
 
-echo "*************************************" 
-echo "* Execute the testcases             *"
-echo "*************************************"
-mvn -e -DskipTests=false -Dtest=EnrichmentInitTests test
-mvn -e -DskipTests=false -Dtest=BasicEnrichmentTests test
-mvn -e -DskipTests=false -Dtest=EnrichmentConfigTests test
-mvn -e -DskipTests=false -Dtest=ASCVDEnrichmentTests test
+   # JUNIT execution reports available in the below folder
+   ls -lrt target/surefire-reports
+   cat target/surefire-reports/categories.EnrichmentInitTests.txt
+   cat target/surefire-reports/categories.BasicEnrichmentTests.txt
+   cat target/surefire-reports/categories.EnrichmentConfigTests.txt
+   cat target/surefire-reports/categories.ASCVDEnrichmentTests.txt
 
-# JUNIT execution reports available in the below folder
-ls -lrt target/surefire-reports
-cat target/surefire-reports/categories.EnrichmentInitTests.txt
-cat target/surefire-reports/categories.BasicEnrichmentTests.txt
-cat target/surefire-reports/categories.EnrichmentConfigTests.txt
-cat target/surefire-reports/categories.ASCVDEnrichmentTests.txt
+elif [ $CLUSTER_NAMESPACE = "clinical-ingestion" ] 
+then
 
+   echo "****************************************************" 
+   echo "* Goto the testcase folder in the repo             *"
+   echo "****************************************************"
+   cd /workspace/$TEST_NAMESPACE/health-patterns/ingest/
+  
+   echo "*************************************" 
+   echo "* Build the testcases               *"
+   echo "*************************************"
+   mvn clean install -e -Dip.fhir=$FHIR_IP -Dport.fhir=$FHIR_PORT -Dip.fhir.proxy=$FHIR_PROXY_IP -Dport.fhir.proxy=$FHIR__PROXY_PORT -Dip.fhir.deid=$FHIR_DEID_IP -Dport.fhir.deid=$FHIR_DEID_PORT -Dip.fhir.deid.proxy=$FHIR_DEID_PROXY_IP -Dport.fhir.deid.proxy=$FHIR_DEID_PROXY_PORT -Dip.deid=$DEID_IP -Dport.deid=$DEID_PORT -Dip.nifi=$NIFI_IP -Dport.nifi=$NIFI_PORT -Dip.expkafka=$EXP_KAFKA_IP -Dport.expkafka=$EXP_KAFKA_PORT -Dpw=$DEFAULT_PASSWORD
+
+   echo "*************************************" 
+   echo "* Initialize the testcases          *"
+   echo "*************************************"
+   mvn -e -DskipTests=false -Dtest=BasicIngestionInitTests test
+
+   echo "*************************************" 
+   echo "* Execute the testcases             *"
+   echo "*************************************"
+   mvn  -e -DskipTests=false -Dtest=FHIRProxyIngestionTests test
+   mvn  -e -DskipTests=false -Dtest=BasicIngestionTests test
+   mvn  -e -DskipTests=false -Dtest=DeIDIngestionTests test
+   mvn  -e -DskipTests=false -Dtest=ASCVDIngestionTests test
+   mvn  -e -DskipTests=false -Dtest=NLPIngestionTests test
+
+   # JUNIT execution reports available in the below folder
+   ls -lrt target/surefire-reports
+   cat target/surefire-reports/categories.BasicIngestionInitTests.txt
+   cat target/surefire-reports/categories.FHIRProxyIngestionTests.txt
+   cat target/surefire-reports/categories.BasicIngestionTests.txt
+   cat target/surefire-reports/categories.DeIDIngestionTests.txt
+   cat target/surefire-reports/categories.ASCVDIngestionTests.txt
+   cat target/surefire-reports/categories.NLPIngestionTests.txt
+   
+fi   
 echo "*************************************" 
 echo "* Report Test Results to Insights   *"
 echo "*************************************"
---
+++ clinical-enrichment/tests/tests/toolchain-envsetup.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+
+# Common toolchain setup code for smoketests, fvttests, and ivttest scripts
+# Input is test type (smoke, fvt, or ivt)
+
+echo "*************************************"
+echo "* Linux version                     *"
+echo "*************************************"
+cat /etc/redhat-release
+
+echo "*************************************"
+echo "* Current Directory                 *"
+echo "*************************************"
+pwd
+
+echo "*************************************"
+echo "* Set Test env variables            *"
+echo "*************************************"
+export DEFAULT_PASSWORD=integrati0n
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0
+export TEST_NAMESPACE=$CLUSTER_NAMESPACE"-"$1
+
+#This is for VPC cluster health-patterns-1
+export INGRESS_SUBDOMAIN=wh-health-patterns.dev.watson-health.ibm.com 
+
+# Set the deploymemt-specific variables/values
+if [ $CLUSTER_NAMESPACE = "clinical-enrich" ]
+then
+   export HELM_RELEASE=enrich
+   export deploywait=120
+elif [ $CLUSTER_NAMESPACE = "clinical-ingestion" ] 
+then
+   export HELM_RELEASE=ingestion
+   export deploywait=300
+fi
+
+echo "*************************************"
+echo "* setup base directory for test     *"
+echo "*************************************"
+cd /workspace
+mkdir $TEST_NAMESPACE
+cd $TEST_NAMESPACE
+
+echo "*************************************"
+echo "* create namespace and set to it    *"
+echo "*************************************"
+kubectl create namespace $TEST_NAMESPACE
+
+echo " Set namespace to $TEST_NAMESPACE"
+kubectl config set-context --current --namespace=$TEST_NAMESPACE
+
+echo "*************************************"
+echo "* Fetch the Code from Repo          *"
+echo "*************************************"
+
+echo " clone clinical ingestion git repo using "$GIT_BRANCH " branch"
+git clone --branch $GIT_BRANCH https://github.com/Alvearie/health-patterns.git
+
+echo " change to the correct directory"
+cd health-patterns/helm-charts/health-patterns
+
+echo " helm dependency update"
+helm3 dependency update
+
+echo " Current Directory:"
+pwd
+
+# Add hostname to values.yaml
+sed -i -e "s/\&hostname replace-me/\&hostname $TEST_NAMESPACE.$INGRESS_SUBDOMAIN/g" values.yaml
+cat values.yaml | grep $TEST_NAMESPACE.$INGRESS_SUBDOMAIN
+
+# setup all the env vars for input to maven
+# FHIR Using INGRESS
+export FHIR_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/fhir
+export FHIR_PORT=
+echo "*************************************"
+echo FHIR server: $FHIR_IP$FHIR_PORT
+echo "*************************************"
+
+# FHIR PROXY Using INGRESS
+export FHIR_PROXY_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/fhir-proxy
+export FHIR__PROXY_PORT=
+echo "*************************************"
+echo FHIR Proxy server: $FHIR_PROXY_IP$FHIR_PROXY_PORT
+echo "*************************************"
+
+# FHIR DEID - using INGRESS
+export FHIR_DEID_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/fhir-deid
+export FHIR_DEID_PORT=
+echo "*************************************"
+echo FHIR DEID server: $FHIR_DEID_IP$FHIR_DEID_PORT
+echo "*************************************"
+
+# FHIR DEID PROXY- using INGRESS
+export FHIR_DEID_PROXY_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/fhir-deid-proxy
+export FHIR_DEID_PROXY_PORT=
+echo "*************************************"
+echo FHIR DEID PROXY server: $FHIR_DEID_PROXY_IP$FHIR_DEID_PROXY_PORT
+echo "*************************************"
+
+# DEID - using INGRESS
+export DEID_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/deid
+export DEID_PORT=
+echo "*************************************"
+echo DEID server: $DEID_IP$DEID_PORT
+echo "*************************************"
+
+# NIFI - using INGRESS
+export NIFI_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/
+export NIFI_PORT=
+echo "*************************************"
+echo NIFI server: $NIFI_IP$NIFI_PORT
+echo "*************************************"
+
+# EXPOSE KAFKA Service using INGRESS
+export EXP_KAFKA_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/expose-kafka
+export EXP_KAFKA_PORT=
+echo "*************************************"
+echo EXPOSE KAFKA Service: $EXP_KAFKA_IP$EXP_KAFKA_PORTT
+echo "*************************************"
+
+# DEID Prep - using INGRESS
+export DEID_PREP_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/deid-prep
+export DEID_PREP_PORT=
+echo "*************************************"
+echo DEID-PREP server: $DEID_PREP_IP$DEID_PREP_PORT
+echo "*************************************"
+
+# TERM Services Prep - using INGRESS
+export TERM_PREP_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/term-services-prep
+export TERM_PREP_PORT=
+echo "*************************************"
+echo TERM-SERVICES-PREP server: $TERM_PREP_IP$TERM_PREP_PORT
+echo "*************************************"
+
+# ASCVD From FHIR Service - using INGRESS
+export ASCVD_FROM_FHIR_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/ascvd-from-fhir
+export ASCVD_FROM_FHIR_PORT=
+echo "*************************************"
+echo ASCVD-FROM-FHIR server: $ASCVD_FROM_FHIR_IP$ASCVD_FROM_FHIR_PORT
+echo "*************************************"
+
+# NLP Insights Service - using INGRESS
+export NLP_INSIGHTS_IP=$TEST_NAMESPACE.$INGRESS_SUBDOMAIN/nlp-insights
+export NLP_INSIGHTS_PORT=
+echo "*************************************"
+echo NLP Insights server: $NLP_INSIGHTS_IP$NLP_INSIGHTS_PORT
+echo "*************************************"
