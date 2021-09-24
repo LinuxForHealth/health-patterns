d01cc6bfbccdcc3ee550a3758050b1a2ebec9cfd
Adam T. Clark <69317102+ibmatc@users.noreply.github.com>
2021-09-24 07:51:06 -0500

    Atc snomed to icd10 (#253)
    * Initial implementation of Snomed-To-ICD10
    
    Signed-off-by: Adam T. Clark <atclark@us.ibm.com>

---
+++ services/snomed-to-icd10/src/main/java/org/alvearie/healthpatterns/SnomedToICD10.java
@@ -0,0 +1,76 @@
+package org.alvearie.healthpatterns;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.structure.Direction;
+import org.apache.tinkerpop.gremlin.structure.Edge;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+
+import com.ibm.fhir.term.graph.FHIRTermGraph;
+import com.ibm.fhir.term.graph.factory.FHIRTermGraphFactory;
+import com.ibm.fhir.term.graph.loader.impl.SnoMedICD10MapTermGraphLoader;
+import com.ibm.fhir.term.graph.loader.util.ConfigLoader;
+
+/**
+ * @author atclark
+ *
+ * This class contains a REST API to connect to a configured Term Graph and retrieve all ICD10 codes for a supplied Snomed code
+ * 
+ * To build, run: ./mvnw package
+ * To run in dev mode, run: ./mvnw compile quarkus:dev  
+ */
+@Path("/")
+public class SnomedToICD10 {
+
+    public static final String CODE = "code";
+    private static final Logger LOG = Logger.getLogger(SnomedToICD10.class.getName());
+
+    private FHIRTermGraph graph = null;
+    private GraphTraversalSource g = null;
+
+    public SnomedToICD10() throws ConfigurationException {
+        Configuration configuration = ConfigLoader.load("conf/janusgraph-cassandra-elasticsearch.properties");
+        graph = FHIRTermGraphFactory.open(configuration);
+        g = graph.traversal();
+    }
+
+    
+    /**
+     * Calculate the ICD10 code(s) for a given Snomed code
+     *
+     * @param snomedCode
+     * @return ICD10 code(s) 
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public final String calculate(@NotNull @QueryParam(CODE) String snomedCode) {
+        try {
+            List<Vertex> snoMedConcepts = g.V().has(CODE, snomedCode).toList();
+            Vertex snoMedConcept = snoMedConcepts.get(0);
+
+            Iterator<Edge> edges = snoMedConcept.edges(Direction.IN, SnoMedICD10MapTermGraphLoader.MAPS_TO);
+            while (edges.hasNext()) {
+                Edge edge = edges.next();
+                Vertex outConcept = edge.outVertex();
+                Object value = outConcept.value(CODE);
+                return value.toString();
+            }
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, "Error finding edge", e);
+        }
+        return "";
+    }
+}
---
+++ services/snomed-to-icd10/src/main/java/org/alvearie/healthpatterns/SnomedToICD10Direct.java
@@ -0,0 +1,72 @@
+package org.alvearie.healthpatterns;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ibm.fhir.term.graph.loader.impl.SnoMedICD10MapTermGraphLoader;
+
+/**
+ * @author atclark
+ *
+ *         This class contains a REST API to retrieve all ICD10 codes for a
+ *         supplied Snomed code. It directly relies on a mapping file downloaded
+ *         from UMLS and stored in cloud object storage (COS).
+ * 
+ *         To build, run: ./mvnw package To run in dev mode, run: ./mvnw compile
+ *         quarkus:dev
+ */
+@Path("/direct")
+public class SnomedToICD10Direct {
+
+    public static final String CODE = "code";
+    private static final String BUCKET_NAME = "fhir-term-graph";
+
+    private static Map<String, Set<String>> SNOMED_TO_ICD = null;
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    /**
+     * This method loads the underlying Snomed->ICD10 map information from file.
+     * 
+     * @return
+     */
+    private static final Map<String, Set<String>> getMap() throws IOException {
+        if (SNOMED_TO_ICD != null) {
+            return SNOMED_TO_ICD;
+        }
+        synchronized (SnomedToICD10Direct.class) {
+            if (SNOMED_TO_ICD != null) {
+                return SNOMED_TO_ICD;
+            }
+            SNOMED_TO_ICD = SnoMedICD10MapTermGraphLoader.loadMap(BUCKET_NAME);
+        }
+        return SNOMED_TO_ICD;
+    }
+
+    /**
+     * Calculate the ICD10 code(s) for a given Snomed code
+     *
+     * @param snomedCode
+     * @return ICD10 code(s)
+     * @throws JsonProcessingException
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public static final String calculate(@NotNull @QueryParam(CODE) String snomedCode) throws IOException, JsonProcessingException {
+        Set<String> icds = new HashSet<>();
+        if (getMap().containsKey(snomedCode)) {
+            icds.addAll(getMap().get(snomedCode));
+        }
+        return mapper.writeValueAsString(icds);
+    }
+}
