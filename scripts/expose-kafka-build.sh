b8e8187c7aec81506740bb57855ca9b0de6a1fb1
Adam T. Clark <atclark@us.ibm.com>
2021-11-17 17:01:08 -0600

    update

--- scripts/expose-kafka-build.sh
+++ scripts/expose-kafka-build.sh
@@ -15,26 +15,6 @@ for i in "$@"; do
       TAG="${i#*=}"
       shift # past argument=value
       ;;
-    -v=*|--version=*)
-      VERSION="${i#*=}"
-      shift # past argument=value
-      ;;
-    -a=*|--tags=*)
-      TAGS="${i#*=}"
-      shift # past argument=value
-      ;;
-    -l=*|--labels=*)
-      LABELS="${i#*=}"
-      shift # past argument=value
-      ;;
-    -j=*|--json=*)
-      JSON="${i#*=}"
-      shift # past argument=value
-      ;;
-    -b=*|--bake-file=*)
-      BAKE-FILE="${i#*=}"
-      shift # past argument=value
-      ;;
     -i=*|--inputs=*)
       INPUTS="${i#*=}"
       shift # past argument=value
@@ -49,11 +29,6 @@ done
 echo "REPOSITORY  = ${REPOSITORY}"
 echo "NAME        = ${NAME}"
 echo "TAG         = ${TAG}"
-echo "VERSION  = ${VERSION}"
-echo "TAGS  = ${TAGS}"
-echo "LABELS  = ${LABELS}"
-echo "JSON  = ${JSON}"
-echo "BAKE-FILE  = ${BAKE-FILE}"
 echo "INPUTS  = ${INPUTS}"
 
 #docker build services/${NAME} -t ${REPOSITORY}/${NAME}:${TAG}
