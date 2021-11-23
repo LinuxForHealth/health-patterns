87392325ce0da71d194d82e1379d9eb7ba424d9c
Adam T. Clark <atclark@us.ibm.com>
2021-11-23 13:37:19 -0600

    update
    Signed-off-by: Adam T. Clark <atclark@us.ibm.com>

--- .github/workflows/scripts/build-service.sh
+++ .github/workflows/scripts/build-service.sh
@@ -217,6 +217,6 @@ fi
 if [ ${MODE} == 'PUSH' ] || [ ${MODE} == 'PR' ]; then
 file="helm-charts/health-patterns/Chart.yaml"
   echo "\nAWK:  awk '!f && s{sub(old,new);f=1}/${REPOSITORY}/{s=1}1' old=\"version: .*\" new=\"version: ${newServiceHelmVer}\" ${file} > ${file}"
-  awk '!f && s{sub(old,new);f=1}/${REPOSITORY}/{s=1}1' old="version: .*" new="version: ${newServiceHelmVer}" ${file} > ${file}
+  awk '!f && s{sub(old,new);f=1}/${REPOSITORY}/{s=1}1' old="version: .*" new="version: ${newServiceHelmVer}" ${file}
   printf "\n\nUpdated ${file} to reflect new helm chart version (${newServiceHelmVer}) for ${REPOSITORY}"
 fi
